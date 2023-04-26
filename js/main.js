de0ffc9e5377e4415ed7e1efebcc203d89a14edf
ho-ji <wkdd0205@gmail.com>
2023-04-26 15:26:43 +0900

    feat: edit memo
    메모 수정 기능 구현

--- js/main.js
+++ js/main.js
@@ -11,19 +11,26 @@ const btnDone = parent.querySelector('.btn-done');
 
 const sectionDisplay = parent.querySelector('.section-display');
 
-/* 입력된 줄 수에 맞춰 textarea 크기 조절 */
-inputTitle.addEventListener('input', e=> {
-  inputTitle.style.height = 'auto';
-  inputTitle.style.height = inputTitle.scrollHeight + 'px';
-});
+const editModal = parent.querySelector('#edit-modal');
+const editModalBox = parent.querySelector('#edit-modal-box');
+const editTitle = parent.querySelector('#edit-title');
+const editContent = parent.querySelector('#edit-content');
+const btnEditColor = parent.querySelector('#btn-edit-color');
+const btnEditRefresh = parent.querySelector('#btn-edit-refresh');
+const btnEditDone = parent.querySelector('#btn-edit-done');
 
-inputContent.addEventListener('input', ()=> {
-  inputContent.style.height = 'auto';
-  inputContent.style.height = inputContent.scrollHeight + 'px';
-});
+/* 입력된 줄 수에 맞춰 textarea 크기 조절 */
+const textArea = parent.querySelectorAll('textarea');
+textArea.forEach(item =>{
+  item.addEventListener('input', ()=>{
+    item.style.height = 'auto';
+    item.style.height = item.scrollHeight + 'px';
+  })
+})
 
 
 parent.addEventListener('click', (e)=>{
+  console.log(e);
   /* 메모 작성 선택 시 확장 */
   if(e.target === inputContent){
     inputTitle.style.display = 'block';
@@ -33,8 +40,9 @@ parent.addEventListener('click', (e)=>{
   /* 배경선택버튼 선택시 */
   else if(e.target === btnColor || e.target.parentElement === btnColor){
     colorPicker.style.display = 'flex';
-    colorPicker.style.left= (btnColor.offsetLeft- 150) + 'px';
-    colorPicker.style.top = (btnColor.offsetTop + 40) + 'px';
+    const pos = btnColor.getBoundingClientRect();
+    colorPicker.style.left= pos.x - 150 +'px';
+    colorPicker.style.top = pos.y + 40 +'px';
   }
   
   /*메모 초기화 버튼 선택 시, 입력된 값 초기화*/
@@ -47,7 +55,10 @@ parent.addEventListener('click', (e)=>{
   /*color-picker 선택 시*/
   else if(e.target === colorPicker || e.target.id.startsWith("btn-color-")){
     if(e.target !== colorPicker){
-      sectionInput.style.backgroundColor = getComputedStyle(e.target).backgroundColor;
+      if(editModal.style.visibility === "hidden")
+        sectionInput.style.backgroundColor = getComputedStyle(e.target).backgroundColor;
+      else
+        editModalBox.style.backgroundColor = getComputedStyle(e.target).backgroundColor;
       for(c of colorPicker.children){
         if(c.classList.contains('color-selected')){
           c.classList.remove('color-selected');
@@ -63,7 +74,10 @@ parent.addEventListener('click', (e)=>{
   /* done 버튼 선택 시*/
   else if(e.target === btnDone || e.target.parentElement === btnDone){
     if(inputTitle.value === "" && inputContent.value === ""){
-      alert('빈 메모는 추가할 수 없습니다!');
+      swal({
+        text: "빈 메모는 만들 수 없습니다!",
+        icon: "info"
+      })
     }
     else{
       saveMemo(inputTitle.value, inputContent.value, getComputedStyle(sectionInput).backgroundColor);
@@ -75,12 +89,41 @@ parent.addEventListener('click', (e)=>{
     }
   }
 
+  /* edit 배경 변경 선택 시*/
+  else if(e.target === btnEditColor || e.target.parentElement == btnEditColor){
+    colorPicker.style.display = 'flex';
+    const pos = btnEditColor.getBoundingClientRect();
+    colorPicker.style.left = pos.x - 150 +'px';
+    colorPicker.style.top = pos.y + 40 +'px';
+    colorPicker.style.position = 'fixed';
+  }
+  /* edit 새로고침 선택 시 */
+  else if(e.target === btnEditRefresh || e.target.parentElement === btnEditRefresh){
+    resetEditText();
+    colorPicker.style.display = 'none';
+    resetColorPicker();
+  }
+
+  /* edit done 선택 시 */
+  else if(e.target === btnEditDone || e.target.parentElement === btnEditDone){
+    if(editTitle.value === "" && editContent.value === ""){
+      swal({
+        text: "빈 메모는 만들 수 없습니다!",
+        icon: "info"
+      })
+    }
+    else{
+      editMemo(editTitle.value, editContent.value, getComputedStyle(editModalBox).backgroundColor, editModalBox.dataset.memoNumber);
+      editModal.style.visibility = "hidden";
+      colorPicker.style.display = 'none';      
+      resetColorPicker();
+    }
+  }
   /*section-input의 내부요소 선택 시, color-picker만 사라짐*/
   else if(e.target.className && sectionInput.querySelector('.'+e.target.className)){
     colorPicker.style.display = 'none';
   }
-  
-  
+
   /*section-input 및 color-picker 외부요소 선택 시, 둘다 사라짐*/
   else{
     inputTitle.style.display = 'none';
@@ -88,6 +131,14 @@ parent.addEventListener('click', (e)=>{
     inputBtnContainer.style.display = 'none';
     colorPicker.style.display = 'none';
     resetColorPicker();
+  }
+
+  /* 수정 모달 영역 외 선택 시, 모달 삭제*/
+  if(editModal.style.visibility !== "hidden" && e.target === editModal){
+    swal({
+      text: "메모를 수정하지 않고 나가겠습니까?",
+      buttons: ["취소", "확인"], 
+    }).then(v=>{if(v) editModal.style.visibility = "hidden";});
   }
 })
 
@@ -105,6 +156,12 @@ const resetInputText = ()=>{
   inputTitle.style.height = 'auto';
   inputContent.style.height = 'auto';
 }
+const resetEditText = () =>{
+  editContent.value = "";
+  editTitle.value = "";
+  editContent.style.height = 'auto';
+  editTitle.style.height = 'auto';
+}
 const resetColorPicker = ()=>{
   for(c of colorPicker.children){
     if(c.classList.contains('color-selected')){
@@ -113,6 +170,7 @@ const resetColorPicker = ()=>{
   }
   colorPicker.children[0].classList.add('color-selected');
   sectionInput.style.backgroundColor = 'white';
+  colorPicker.style.position = 'absolute';
 }
 
 const saveMemo = (title, content, bc) => {
@@ -122,6 +180,13 @@ const saveMemo = (title, content, bc) => {
   localStorage.setItem("bc"+ n, bc);
   render();
 };
+
+const editMemo = (title, content, bc, n) => {
+  localStorage.setItem("title"+ n, title);
+  localStorage.setItem("content"+ n, content);
+  localStorage.setItem("bc"+ n, bc);
+  render();
+}
 
 const render = () => {
   sectionDisplay.innerHTML = "";
@@ -138,10 +203,10 @@ const render = () => {
 
       if(memoTitle.textContent !== "")
         memoTitle.setAttribute("style", "font-weight: 600; margin-bottom: 10px");
-      memoTitle.setAttribute("data-memo-number", n)
-      memoContent.setAttribute("style", "margin-bottom: 10px");
-      memoItem.setAttribute("style", "background-color:" + bc);
-      memoItem.setAttribute("class", "display-memo");
+        memoContent.setAttribute("style", "margin-bottom: 10px");
+        memoItem.setAttribute("style", "background-color:" + bc);
+        memoItem.setAttribute("class", "display-memo");
+        memoItem.setAttribute("data-memo-number", n)
 
       memoItem.appendChild(memoTitle);
       memoItem.appendChild(memoContent);
@@ -155,5 +220,22 @@ const render = () => {
     messageEmptyMemo.setAttribute("style", "font-size: 1.5rem; margin:100px auto 0px auto; color: #cccccc;");
     sectionDisplay.appendChild(messageEmptyMemo);
   }
+  /*메모가 렌더링 될때마다 이벤트 리스너추가*/
+  const memos = document.querySelectorAll('.display-memo');
+  memos.forEach(item =>
+    item.addEventListener('click', (e)=>{
+      const memoNumber = item.dataset.memoNumber;
+      editModal.style.visibility = "visible";
+
+      editTitle.value = localStorage.getItem("title"+memoNumber);
+      editTitle.style.height = editTitle.scrollHeight + 'px';
+
+      editContent.value = localStorage.getItem("content"+memoNumber);
+      editContent.style.height = editContent.scrollHeight + 'px';
+
+      editModalBox.style.backgroundColor = localStorage.getItem("bc"+memoNumber);
+      
+      editModalBox.setAttribute('data-memo-number', memoNumber);
+  }))
 };
 render();
