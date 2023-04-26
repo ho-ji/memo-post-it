0795c7fec20e2726dbb6d3dd6657bc46882c6d25
ho-ji <wkdd0205@gmail.com>
2023-04-26 10:55:12 +0900

    feat: add done click and render
    메모 작성 완료 버튼이 눌렸을 때 및 메모 리스트 렌더 추가

--- js/main.js
+++ js/main.js
@@ -9,6 +9,8 @@ const colorPicker = parent.querySelector('.color-picker');
 const btnRefresh = parent.querySelector('.btn-refresh');
 const btnDone = parent.querySelector('.btn-done');
 
+const sectionDisplay = parent.querySelector('.section-display');
+
 /* 입력된 줄 수에 맞춰 textarea 크기 조절 */
 inputTitle.addEventListener('input', e=> {
   inputTitle.style.height = 'auto';
@@ -58,6 +60,21 @@ parent.addEventListener('click', (e)=>{
     colorPicker.style.display = 'none';
   }
   
+  /* done 버튼 선택 시*/
+  else if(e.target === btnDone || e.target.parentElement === btnDone){
+    if(inputTitle.value === "" && inputContent.value === ""){
+      alert('빈 메모는 추가할 수 없습니다!');
+    }
+    else{
+      saveMemo(inputTitle.value, inputContent.value, getComputedStyle(sectionInput).backgroundColor);
+      inputTitle.style.display = 'none';
+      resetInputText();
+      inputBtnContainer.style.display = 'none';
+      colorPicker.style.display = 'none';      
+      resetColorPicker();
+    }
+  }
+
   /*section-input의 내부요소 선택 시, color-picker만 사라짐*/
   else if(e.target.className && sectionInput.querySelector('.'+e.target.className)){
     colorPicker.style.display = 'none';
@@ -74,11 +91,12 @@ parent.addEventListener('click', (e)=>{
   }
 })
 
-/* 화면 크기 변화 시 color-picker 지우기 */
+/* 화면 크기 변화 시 color-picker 지우기 및 메모 재렌더링*/
 window.addEventListener('resize',function(){
   if(colorPicker.style.display !== 'none'){
     colorPicker.style.display = 'none';
   }
+  render();
 })
 
 const resetInputText = ()=>{
@@ -97,3 +115,45 @@ const resetColorPicker = ()=>{
   sectionInput.style.backgroundColor = 'white';
 }
 
+const saveMemo = (title, content, bc) => {
+  const n = localStorage.length / 3;
+  localStorage.setItem("title"+ n, title);
+  localStorage.setItem("content"+ n, content);
+  localStorage.setItem("bc"+ n, bc);
+  render();
+};
+
+const render = () => {
+  sectionDisplay.innerHTML = "";
+  if(localStorage.length !== 0){
+    let memoCnt = localStorage.length / 3;
+    for(let n = memoCnt - 1; n >= 0; n--){
+      let memoItem = document.createElement('div');
+      let memoTitle = document.createElement('p');
+      let memoContent = document.createElement('p');
+      
+      memoTitle.textContent = localStorage.getItem("title"+ n);
+      memoContent.textContent = localStorage.getItem("content"+ n);
+      let bc = localStorage.getItem("bc" + n);
+
+      if(memoTitle.textContent !== "")
+        memoTitle.setAttribute("style", "font-weight: 600; margin-bottom: 10px");
+      memoTitle.setAttribute("data-memo-number", n)
+      memoContent.setAttribute("style", "margin-bottom: 10px");
+      memoItem.setAttribute("style", "background-color:" + bc);
+      memoItem.setAttribute("class", "display-memo");
+
+      memoItem.appendChild(memoTitle);
+      memoItem.appendChild(memoContent);
+
+      sectionDisplay.appendChild(memoItem);
+    }
+  }
+  else{
+    const messageEmptyMemo = document.createElement('p');
+    messageEmptyMemo.textContent = "[메모장이 비어있습니다]";
+    messageEmptyMemo.setAttribute("style", "font-size: 1.5rem; margin:100px auto 0px auto; color: #cccccc;");
+    sectionDisplay.appendChild(messageEmptyMemo);
+  }
+};
+render();
